68dc7c7df0a0732e5a86f7a6f1d1a0b9183d851b
czawora <christopher.zawora@gmail.com>
2019-05-16 10:21:42 -0400

    add more mem to reren spikeInfo

--- tally_sessions.py
+++ tally_sessions.py
@@ -217,7 +217,7 @@ if rerun:
     spikeInfo_rerun_big_bash_file = swarms_path + "/sort_rerun_spikeInfo_big_bash.sh"
     spikeInfo_rerun_swarm_file = swarms_path + "/sort_rerun_spikeInfo_swarm.sh"
 
-    rerun_sort_swarm_command = "swarm -g 150 -b 1 -t 2 --time 10:00:00 --gres=lscratch:15 --merge-output --logdir "
+    rerun_sort_swarm_command = "swarm -g 200 -b 1 -t 2 --time 10:00:00 --gres=lscratch:15 --merge-output --logdir "
     rerun_sort_swarm_command += swarms_path + "/log_dump"
     rerun_sort_swarm_command += " -f " + spikeInfo_rerun_big_bash_file
 
