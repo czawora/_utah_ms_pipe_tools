ab02eb9c030fa54627b36185cee033376a6a6d2e
czawora <christopher.zawora@gmail.com>
2019-09-10 16:47:24 -0400

    add fresh_rerun

--- tally_sessions.py
+++ tally_sessions.py
@@ -300,5 +300,5 @@ if rerun or fresh_rerun:
 
         rerun_sesslist.close()
 
-        print("recreating bash scripts for " + str(len(incomplete_chans)) + " sessions ")
+        print("recreating bash scripts for " + str(len(incomplete_chans_sess)) + " sessions ")
         call(["python3", spikes_pipeline_dir + "/construct_bash_scripts.py", subj_path, "--sesslist", rerun_sesslist_fname, "--output_suffix", "rerun", "--fresh_write"])
